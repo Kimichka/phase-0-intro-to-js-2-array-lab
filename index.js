19cc027eaa5cd660c5d545aeeb0945a191ce1d45
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-03-04 22:47:37 -0500

    Adds two files

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+cats.push(name)
+return cats
+}
+
+//cats.length = 0;
+//cats.push("Milo", "Otis", "Garfield");
+
+function destructivelyPrependCat(name){
+cats.unshift(name)
+return cats
+}
+
+function destructivelyRemoveLastCat(){
+cats.pop()
+return cats
+}
+
+function destructivelyRemoveFirstCat(){
+cats.shift()
+return cats
+}
+
+function appendCat(name){
+var name_1= [...cats,name];
+return name_1;
+}
+
+
+function prependCat(name){
+var name_2=[name, ...cats];
+return name_2
+}
+  
+function removeLastCat(){
+var cats_1= cats.slice(0,cats.length-1);
+return cats_1;
+}
+  
+function removeFirstCat(){
+var cats_2= cats.slice(1)
+return cats_2
+}
